b72f67f741e0ab7045f94b5e1b848d37a3a1f0d3
Naimur <naimur58co@gmail.com>
2023-06-10 19:21:25 +0600

    function

---
+++ main.js
@@ -0,0 +1,25 @@
+const toDos = [
+    {
+        "id": 1,
+        "text": "Take the trash",
+        "status":true
+    },
+    {
+        "id": 2,
+        "text": "Meeting with boss",
+        "status":true
+    },
+    {
+        "id": 1,
+        "text": "Doctor appt",
+        "status":false
+    }
+]
+
+const statusCheck = toDos.filter(function(todo){
+    return todo.status === true;
+}).map(function(todo){
+    console.log( todo.text);
+})
+// console.log(statusCheck)
+
